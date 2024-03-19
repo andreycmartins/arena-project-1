7deb7d6b7e8c49c49e6afa188eaa4371b37aa5aa
Andrey Martins <andrey.costa@versatecnologia.com.br>
2024-03-19 13:47:30 -0300

    em andamento

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,26 +1,9 @@
 'use client'
 
-import { SideNav } from '@/components/side-nav'
-import { useSidebar } from '@/hooks/useSidebar'
-import { NavItem } from '@/types'
-import { ChevronDown } from 'lucide-react'
-import { useSession } from 'next-auth/react'
-import React from 'react'
-
 export default function Admin() {
-  const { data } = useSession()
-  const { isOpen } = useSidebar()
-  const item: NavItem[] = [
-    {
-      title: 'abrir google',
-      href: '/google.com',
-      icon: ChevronDown,
-    },
-  ]
-  console.log(data)
   return (
     <div>
-      <SideNav setOpen={isOpen} items={item} className="mt-5" />
+      <h1>Admin page</h1>
     </div>
   )
 }
